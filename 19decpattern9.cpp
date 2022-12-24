0df86948722b623caa4228d7244591c64572e782
rishikeshbodkhe68 <118285751+rishikeshbodkhe68@users.noreply.github.com>
2022-12-24 13:07:57 +0530

    Add files via upload

---
+++ 19decpattern9.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int h,sp,str;
+	for(h='A';h<='G';h=h+2)
+	{
+		for(sp=134-h;sp>=1;sp--)
+		{
+			printf(" ");
+		}
+		for(str=h;str>='A';str--)
+		{
+			printf("%c",str);
+		}
+		printf("\n");
+
+		
+	}
+	
+	
+	
+}
